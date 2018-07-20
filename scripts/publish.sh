ea51e1b08b64e97bc3e61f147c786b9ea36f0729
Mauricio Togneri <mtogneri@openwt.com>
2018-07-20 10:03:36 +0200

    Using Gradle task to publish

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -4,5 +4,4 @@ set -e
 
 DIR=`dirname $0`
 $DIR/../gradlew assembleRelease
-
-java -jar $DIR/android-publisher.jar -package com.mauriciotogneri.testpublish -email ${TEST_PUBLISH_SERVICE_ACCOUNT_EMAIL} -p12 ${TEST_PUBLISH_SERVICE_ACCOUNT_P12} -apk $DIR/../app/build/outputs/apk/release/app-release.apk -track internal
+$DIR/../gradlew publish -PpackageName="com.mauriciotogneri.testpublish" -PapkPath="$DIR/../app/build/outputs/apk/release/app-release.apk" -PtrackName="internal"
